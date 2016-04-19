018b1b2e2f12542d1b860ff5519c7cc1920754b2
saarnii1 <saarnii1@topaasi.org.aalto.fi>
2016-04-19 15:39:41 +0300

    Lisätty metaakoodi find_recipesiin

--- find_recipes.py
+++ find_recipes.py
@@ -3,3 +3,54 @@ Created on Apr 19, 2016
 
 @author: saarnii1
 '''
+class read_inventory:
+    '''
+    Katsotaan varasto läpi eli selvitetään mitä kaikkia raaka-aineita on käytettävissä
+    '''
+    try:
+        with open('inventory') as inventory:
+            lineCount = 0
+            characterCount = 0
+            for line in inventory:
+                '''
+                line_tmp = line.split('!')      #tiedostossa siis raaka-aine, sen määrä ja muut tiedot erotettu huutomerkillä
+                line_tmp[0] = ingredient
+                line_tmp[1] = amount
+                line_tmp[2] = unit
+                line_tmp[3] = reseptit
+                line_tmp[4] = allergeenit
+                '''
+                
+                
+    except OSError:
+        print("Could not open inventory")
+    
+
+
+class find_recipes:
+    
+    '''
+    Eli etsi_ruoka
+    Määritellää tarkemmin sitä, mitä lähdetään hakemaan:
+    määritellään ehdot sille, millä ehdoilla ohjelman kehittämä lista halutaan muodostaa.
+    '''
+        
+    def no_conditions(self):
+        '''
+        etsi reseptit, kunkäyttäjä ei aseta ehtoja ruuan raaka-aineista ja määristä
+        '''
+        
+    def missing_n_ingredients(self):
+        '''
+        etsi reseptit, joiden valmistamiseksi puuttuu korkeintaa n raaka-ainetta
+        '''
+        
+    def must_not_include(self):
+        '''
+        etsi reseptit, jotka eivät saa sisältää tiettyö raaka-ainetta
+        '''
+    
+    def must_include(self):
+        '''
+        etsi reseptit, joiden halutaan sisältävän tiettyä raaka-ainetta
+        '''
