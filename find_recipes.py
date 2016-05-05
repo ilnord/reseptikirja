f143d2db4586d3363cdc3e2c216046cf56c25a79
Ilkka <ilkka.saarnilehto@aalto.fi>
2016-05-05 12:18:06 +0300

    Filusta luku toimii. Hakuun duunattu jotain pientä kivaa. Commitin tarkoituksena toimia paluupisteena seuraavaksi tehtavien "valmiiden" luokkien muuttamisen turvana

--- find_recipes.py
+++ find_recipes.py
@@ -3,32 +3,79 @@ Created on Apr 19, 2016
 @author: saarnii1
 '''
 # -*- coding: utf-8 -*-
-            
 
-class find_recipes:
-    
+from recipe import Recipe
+from IO import IO         
+from ingredient import Ingredient
+from unit_transfer import Unit_transfer
+
+class Find_recipes:
     '''
     Eli etsi_ruoka
-    Määritellää tarkemmin sitä, mitä lähdetään hakemaan:
-    määritellään ehdot sille, millä ehdoilla ohjelman kehittämä lista halutaan muodostaa.
+    Maaritellaan tarkemmin sita, mita lahdetaan hakemaan:
+    maaritellaan milla ehdoilla ohjelman kehittama lista halutaan muodostaa.
     '''
+    
+    def __init__(self):
+        f_storage = open('Storagelist.txt', 'r')
+        f_recipe = open('Recipelist.txt', 'r')
+        self.storage_list = self.IO.read_storage_from_file(f_storage)
+        self.recipes_list = self.IO.read_recipes_from_file(f_recipe)
+        f_storage.close()
+        f_recipe.close()
+        self.makeable_recipes = []
+       
+ 
+    def find_all_recipes(self):
+        for recipe in self.recipes_list:
+            if len(recipe.get_ingredients()) == self.check_for_ingredients(recipe):
+                self.makeable_recipes.append(recipe)
+        return self.makeable_recipes
         
-    def no_conditions(self):
-        '''
-        etsi reseptit, kunkäyttäjä ei aseta ehtoja ruuan raaka-aineista ja määristä
-        '''
+    
+     
+    def check_ingredient_amount(self, recipe_ingredient, storage_ingredient):
+        if recipe_ingredient.get_unit() == storage_ingredient.get_unit():
+            if recipe_ingredient.get_amount() < storage_ingredient.get_amount():
+                return True
+            else:
+                return False
+        else:
+            wanted_unit = recipe_ingredient.get_unit
+            received_unit = storage_ingredient.get_unit
+            density = recipe_ingredient.get_density()
+            received_amount = storage_ingredient.get_amount
+            storage_ingredient_transferred_amount = Unit_transfer.unit_transfer(self, received_unit, wanted_unit, received_amount, density)
+            if recipe_ingredient.get_amount() < storage_ingredient_transferred_amount:
+                return True
+            else:
+                return False
         
+        
+    def check_for_ingredients(self, Recipe):
+        ingredients_found = 0
+        
+        for recipe_ingredient in Recipe.get_ingredients():
+            for storage_ingredient in self.storage_list:
+                if recipe_ingredient.get_ingredient == storage_ingredient.get_ingredient:
+                    if self.check_ingredient_amount(recipe_ingredient, storage_ingredient):
+                        ingredients_found += 1
+                    elif recipe_ingredient.get_recipes() != None:
+                        if self.check_for_ingredients(recipe_ingredient.get_recipes()) \
+                        == len(recipe_ingredient.get_recipes().get_ingredients()):
+                            ingredients_found += 1
+                    break
+        return ingredients_found
+                    
+    
     def missing_n_ingredients(self):
-        '''
-        etsi reseptit, joiden valmistamiseksi puuttuu korkeintaa n raaka-ainetta
-        '''
+        #etsi reseptit, joiden valmistamiseksi puuttuu korkeintaa n raaka-ainetta
+        pass
         
     def must_not_include(self):
-        '''
-        etsi reseptit, jotka eivät saa sisältää tiettyö raaka-ainetta
-        '''
+        #etsi reseptit, jotka eivat saa sisaltaa tiettya raaka-ainetta
+        pass
     
     def must_include(self):
-        '''
-        etsi reseptit, joiden halutaan sisältävän tiettyä raaka-ainetta
-        '''
+        #etsi reseptit, joiden halutaan sisaltavan tiettya raaka-ainetta
+        pass
--- test.py
+++ test.py
@@ -17,9 +17,11 @@ PORTION = 8
 import unittest
 from main_menu import Main
 from IO import IO
-from ingredient import Ingredient
+from ingredient import Ingredient, Ingredient_container
 from io import StringIO
+from recipe import Recipe
 from unit_transfer import Unit_transfer
+from find_recipes import Find_recipes
 
 class Test(unittest.TestCase):
     
@@ -31,6 +33,7 @@ class Test(unittest.TestCase):
         #IO setUp
         self.IO = IO()
         self.Unit_transfer = Unit_transfer()
+        self.Find_recipes = Find_recipes
         
     def test_transfer_mass_to_mass(self):
         unit = KG 
@@ -101,7 +104,19 @@ class Test(unittest.TestCase):
             self.assertEqual("Kala", ingredient.get_name(), "Raaka-aineen nimi ei tasmaa")
             self.assertEqual(3, ingredient.get_density(), "Raaka-aineen tiheys ei tasmaa")
             self.assertEqual(["Kala","Ruodot"], ingredient.get_allergens(), "")
-
+            
+    def test_read_ingredients_from_actual_file(self):
+        f = open('Ingredientlist.txt', 'r')
+        ingredients_list, successfull_reads, failed_reads = self.IO.read_ingredients_from_file(f)
+        f.close()
+        self.assertEqual(1, successfull_reads,"Onnistuneiden lukujen maara ei tasmaa")
+        self.assertEqual(1, failed_reads, "Epaonnistuneiden lukujen maara ei tasmaa")
+        self.assertEqual(successfull_reads, len(ingredients_list), "Listan pituus ei vastaa onnistuneita lukuja")
+        if len(ingredients_list) > 0:
+            ingredient = ingredients_list[0]
+            self.assertEqual("Kala", ingredient.get_name(), "Raaka-aineen nimi ei tasmaa")
+            self.assertEqual(3, ingredient.get_density(), "Raaka-aineen tiheys ei tasmaa")
+            self.assertEqual(["Kala","Ruodot"], ingredient.get_allergens(), "")
 
     def test_read_recipes_from_file(self):
          
@@ -146,7 +161,7 @@ class Test(unittest.TestCase):
         self.input_file = StringIO()
         self.input_file.write('STORAGELIST\n')
         self.input_file.write('riisi;10;kg\n')
-        self.input_file.write('raakisett\n')
+        self.input_file.write('asdpoks\n')
         self.input_file.write('mehu;1;dl\n')
 
         self.input_file.seek(0, 0) 
@@ -171,3 +186,35 @@ class Test(unittest.TestCase):
         if len(storage_list) > 0:
             self.assertIs(ingredient1, storage_list[0].get_ingredients(), "Ensimmainen raaka-aine ei tasmaa haluttua olioita")
             self.assertIs(ingredient2, storage_list[1].get_ingredients(), "Toinen raaka-aine ei tasmaa haluttua oliota")
+            
+            
+###################################################################################################################
+
+    def test_check_for_ingredients(self):
+        
+        ingredient1 = Ingredient()
+        ingredient1.set_name("riisi")
+        ingredient1.set_density(0.5)
+        ingredient1
+        
+        ingredient2 = Ingredient()
+        ingredient2.set_name("vesi")
+        ingredient2.set_density(1)
+        
+        ingredients_list = [ingredient1,ingredient2]
+        
+        Ingredient_container.set_ingredient(self, ingredient1, ingredients_list)
+        Ingredient_container.set_ingredient(self, ingredient2, ingredients_list)
+        
+
+        recipe1 = Recipe()
+        recipe1.set_name("Paistettu riisi")
+        recipe1.set_outcome_amount(5)
+        recipe1.set_outcome_unit("dl")
+        recipe1.set_instructions("keita riisi")
+        recipe1.set_instructions("paista riisi")
+        recipe1.add_ingredients(Ingredient_container)
+
+        
+        ingredients_found = self.Find_recipes.check_for_ingredients(recipe1)
+        self.assertEqual(1, ingredients_found,"Loytyneiden raaka-aineiden maara ei tasmaa")
